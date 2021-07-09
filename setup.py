080ac77d0fa886e49963c912a528f777ffdd922c
manavkant <mkant@caltech.edu>
2021-07-08 17:31:35 -0700

    Increase deepcell-toolbox version

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
         'jupyter>=1.0.0,<2',
         'opencv-python-headless<5',
         'deepcell-tracking>=0.4.0',
-        'deepcell-toolbox>=0.9.0,<0.10.0'
+        'deepcell-toolbox>0.10.0'
     ],
     extras_require={
         'tests': [
