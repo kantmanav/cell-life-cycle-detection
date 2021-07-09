2271c8094d996c214e5c231e8835280582bc709e
manavkant <mkant@caltech.edu>
2021-07-09 14:01:52 -0700

    Add deepcell-tf master branch from git

--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ setup(
         'tensorflow~=2.4.2',
         'tensorflow-addons>=0.13.0',
         'jupyter>=1.0.0,<2',
+        'git+https://github.com/vanvalenlab/deepcell-tf.git@master',
         'opencv-python-headless<5',
         'deepcell-tracking>=0.4.0',
         'deepcell-toolbox>0.10.0'
